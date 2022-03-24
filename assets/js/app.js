ff6073c0b97ec1158063e005440229c0baffb1f7
Nenad <n.markovic.kg.95@gmail.com>
2022-03-24 18:55:04 +0100

    UI class

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ const cartOverlay=document.querySelector('cart-overlay');
 const cartItems=document.querySelector('cart-items');
 const cartTotal=document.querySelector('cart-total');
 const cartContent=document.querySelector('cart-content');
-const productsDOM=document.querySelector('products');
+const productsDOM=document.querySelector('products-center');
 
 //korpa
 class Products{
@@ -34,10 +34,17 @@ class UI{
          <!--Jedan proizvod-->
         <article class="products">
          <div class="img-container">
-
+             <img src=${product.product_image} alt="product" class="product-img"/>
+             <button class="bag-btn" data-id=${product.product_id}>
+             <i class="fas fa-shoping-cart"></i>
+             Add to cart
+             </button>
          </div>
+         <h3>${product.product_name}</h3>
+         <h4>${product.product_price}</h4>
          </article>
          `
      });
+     productsDOM.innerHTML=result;
     }
 }
